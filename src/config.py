cd9b64eedd485801404e5c84067729493231436e
Sean Ryan <str_ie@yahoo.co.uk>
2024-06-02 21:01:54 +0200

    Minor tweaks

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 DOCS_LOCATION = "./data"
+
 EMBEDDING_MODEL = "nomic-embed-text"
 EMBEDDING_MODEL_BAAI = "BAAI/bge-small-en-v1.5"  # TODO is this more efficient than nomic-embed-text?
 
@@ -8,3 +9,7 @@ TEMPERATURE = 0.7
 # For performance, try disabling one of these:
 IS_SUMMARY_ENABLED = False
 IS_VECTOR_ENABLED = True
+
+CLEAR_OUT_INDEX = False  # normally False unless code has changed how storage (local or redis) is used
+
+REDIS_SIMILARITY_TOP_K=10  # higher lists more result-docs, but may be slower
--- src/main_redis.py
+++ src/main_redis.py
@@ -1,6 +1,12 @@
 # ref https://docs.llamaindex.ai/en/stable/examples/ingestion/ingestion_gdrive/
+# ref https://docs.llamaindex.ai/en/stable/examples/ingestion/redis_ingestion_pipeline/
 
-from cornsnake import util_print
+import os
+
+from cornsnake import util_print, util_color, util_input
+
+from cornsnake import config as corsnake_config
+corsnake_config.IS_INTERACTIVE = True
 
 # built-in data reader - consumes text and PDF files
 from llama_index.core import SimpleDirectoryReader
@@ -37,7 +43,7 @@ Settings.embed_model = OllamaEmbedding(model_name=config.EMBEDDING_MODEL)
 
 custom_schema = IndexSchema.from_dict(
     {
-        "index": {"name": "gdrive", "prefix": "doc"},
+        "index": {"name": "redis_vector_store", "prefix": "doc"},
         # customize fields that are indexed
         "fields": [
             # required fields for llamaindex
@@ -66,8 +72,9 @@ vector_store = RedisVectorStore(
 )
 
 # Optional: clear vector store if exists
-# if vector_store.index_exists():
-#     vector_store.delete_index()
+if config.CLEAR_OUT_INDEX and vector_store.index_exists():
+    util_print.print_section("Clearing out index")
+    vector_store.delete_index()
 
 # Set up the ingestion cache layer
 cache = IngestionCache(
@@ -83,29 +90,44 @@ pipeline = IngestionPipeline(
     docstore=RedisDocumentStore.from_host_and_port(
         "localhost", 6379, namespace="document_store"
     ),
-    vector_store=vector_store,
-    cache=cache,
+    vector_store=RedisVectorStore(
+        schema=custom_schema,
+        redis_url="redis://localhost:6379",
+    ),
+    cache=IngestionCache(
+        cache=RedisCache.from_host_and_port("localhost", 6379),
+        collection="redis_cache",
+    ),
     docstore_strategy=DocstoreStrategy.UPSERTS,
 )
 
 from llama_index.core import VectorStoreIndex
 
-# TODO try SummaryIndex
 vector_index = VectorStoreIndex.from_vector_store(
     pipeline.vector_store, embed_model=embed_model
 )
 
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 util_print.print_section(f"Reading documents from {config.DOCS_LOCATION}")
-documents = SimpleDirectoryReader(config.DOCS_LOCATION).load_data()
+documents = SimpleDirectoryReader(config.DOCS_LOCATION, filename_as_id=True).load_data()
 
 nodes = pipeline.run(documents=documents)
 print(f"Ingested {len(nodes)} Nodes")
 
-query_engine = vector_index.as_query_engine()
+query_engine = vector_index.as_query_engine(similarity_top_k=config.REDIS_SIMILARITY_TOP_K)
+
+def _display_response_sources(source_nodes):
+    for node in source_nodes:
+        print(f" - [{node.metadata['file_path']}] [score: {round(node.score, 2)}] [{node.metadata['last_modified_date']}]")
+    if len(source_nodes) == config.REDIS_SIMILARITY_TOP_K:
+        util_print.print_with_color("note: there may be more documents available - please ask a more specific question, or edit config.py setting REDIS_SIMILARITY_TOP_K", util_color.CONFIG_COLOR)
 
 
 def _display_response(response):
     print(response)
+    if response.source_nodes:
+        _display_response_sources(response.source_nodes)
 
 
 util_print.print_section("Starting query loop...")
@@ -117,13 +139,10 @@ from tenacity import retry, wait_fixed, stop_after_attempt
 def send_prompt(user_query):
     return query_engine.query(user_query)
 
-
 USER_EXIT = "bye"
 
 while True:
-    user_query = input(
-        "How can I help? [to exit, type 'bye' and press ENTER] [for a summary, say 'summary'] >>"
-    )
+    user_query = util_input.input_required("How can I help? [to exit, type 'bye' and press ENTER] [for a summary, say 'summary' or 'What documents do you see?'] >>", "")
     if user_query.lower() == USER_EXIT.lower():
         print("Goodbye for now")
         break
